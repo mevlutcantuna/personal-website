8c1edf141234a5401364e1a9f5a07fd60d8686dc
mevlutcantuna <mttuna90@gmail.com>
2022-05-06 16:16:03 +0300

    added font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "source": ["Source Sans Pro","sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
