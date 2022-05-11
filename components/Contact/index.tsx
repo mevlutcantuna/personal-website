64e6644c197a0ec72867bec0dd565123f9021560
mevlutcantuna <mttuna90@gmail.com>
2022-05-11 11:37:33 +0300

    contant backend completed

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import styles from "./Contact.module.css"
 
+import emailjs from '@emailjs/browser';
+import { message } from 'antd';
+
 const Contact: React.FC = () => {
+    const form = useRef<any>();
+    const [formValues, setFormValues] = useState({ full_name: "", email: "", message: "" })
+
+    const handleFormChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
 
     const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+
+        if (formValues.full_name.trim() === "" || formValues.email.trim() === "" || formValues.message.trim() === "") {
+            return message.error("please provide all inputs..")
+        }
+
+        emailjs.sendForm('service_sc6l8zt', 'personal_website', form.current, 'bzbmoEATFTS3HTzIq')
+            .then((result) => {
+                setFormValues({ ...formValues, full_name: "", email: "", message: "" })
+                return message.success("your message sended.i'll ")
+            }, (error) => {
+                alert(error.text);
+            });
     }
 
 
@@ -24,11 +45,11 @@ const Contact: React.FC = () => {
             </div>
             <div data-aos="fade-down"
                 data-aos-anchor-placement="top-bottom" className={styles.contact__form}>
-                <form onSubmit={sendMessage}>
-                    <input type="text" placeholder="Full Name" />
-                    <input type="email" placeholder="Email" />
-                    <textarea placeholder="Your Message" />
-                    <button>{"Let's Talk"}</button>
+                <form ref={form} onSubmit={sendMessage}>
+                    <input value={formValues.full_name} onChange={handleFormChange} type="text" placeholder="Full Name" name="full_name" />
+                    <input value={formValues.email} onChange={handleFormChange} type="email" placeholder="Email" name="email" />
+                    <textarea value={formValues.message} onChange={handleFormChange} placeholder="Your Message" name="message" />
+                    <button type="submit" value="Send">{"Let's Talk"}</button>
                 </form>
             </div>
         </div>
